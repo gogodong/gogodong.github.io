c9257427b2da4587dd449165bd927570157b3c38
guozhendong <1301646379@qq.com>
2025-11-20 14:05:28 +0800

    Site updated: 2025-11-20 14:05:28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1763614207284";const preCache = ["/images/taichi.png","/images/banner_fulilian.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1763614759606";const preCache = ["/images/taichi.png","/images/banner_fulilian.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
